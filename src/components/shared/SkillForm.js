902cf4ee1374cd66e02a6bbb70cf9c9ab43548ab
kurotsubasa <kurotsubasa10@gmail.com>
2020-04-02 17:42:38 -0400

    Added radio button instead of input for resources
    Added radio buttons instead of input for resources in forms

--- src/components/shared/SkillForm.js
+++ src/components/shared/SkillForm.js
@@ -3,46 +3,66 @@ import { Link } from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-const ForagerForm = ({ skill, handleSubmit, handleChange, cancelPath }) => (
-  <Form onSubmit={handleSubmit}>
-    <Form.Label>Name</Form.Label>
-    <Form.Control
-      placeholder="A Great Skill"
-      value={skill.name}
-      name="name"
-      onChange={handleChange}
-    />
+const ForagerForm = ({ skill, handleSubmit, handleChange, cancelPath }) => {
+  const resources = ['mp', 'str']
+  const resourceJSX = resources.map(resource => {
+    let isChecked = false
+    if (resource === skill.resource) {
+      isChecked = true
+    }
 
-    <Form.Label>Description</Form.Label>
-    <Form.Control
-      placeholder="John Doe"
-      value={skill.description}
-      name="description"
-      onChange={handleChange}
-    />
+    return (
+      <fieldset key={resource} className="resources">
+        <label htmlFor={resource}>{resource}</label>
+        <input
+          type="radio"
+          id={resource}
+          value={resource || ''}
+          name="resource"
+          onChange={handleChange}
+          checked={isChecked}
+        />
+      </fieldset>
+    )
+  })
 
-    <Form.Label>Resource</Form.Label>
-    <Form.Control
-      placeholder="hp"
-      value={skill.resource}
-      name="resource"
-      onChange={handleChange}
-    />
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Label>Name</Form.Label>
+      <Form.Control
+        placeholder="A Great Skill"
+        value={skill.name}
+        name="name"
+        onChange={handleChange}
+      />
 
-    <Form.Label>Cost</Form.Label>
-    <Form.Control
-      type='Number'
-      placeholder="00"
-      value={skill.cost}
-      name="cost"
-      onChange={handleChange}
-    />
+      <Form.Label>Description</Form.Label>
+      <Form.Control
+        placeholder="John Doe"
+        value={skill.description}
+        name="description"
+        onChange={handleChange}
+      />
 
-    <Button type="submit">Submit</Button>
-    <Link to={cancelPath}>
-      <Button>Cancel</Button>
-    </Link>
-  </Form>
-)
+      <fieldset>
+        {resourceJSX}
+      </fieldset>
+
+      <Form.Label>Cost</Form.Label>
+      <Form.Control
+        type='Number'
+        placeholder="00"
+        value={skill.cost}
+        name="cost"
+        onChange={handleChange}
+      />
+
+      <Button type="submit">Submit</Button>
+      <Link to={cancelPath}>
+        <Button>Cancel</Button>
+      </Link>
+    </Form>
+  )
+}
 
 export default ForagerForm
