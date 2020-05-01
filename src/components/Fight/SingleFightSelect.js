bcd99dad7676ff0aa2cfbcb1082a838856cd22da
kurotsubasa <kurotsubasa10@gmail.com>
2020-05-01 13:08:30 -0400

    Add in axios call and function for foragers
    Add axios call and function for foragers, yet to finish select

--- src/components/Fight/SingleFightSelect.js
+++ src/components/Fight/SingleFightSelect.js
@@ -6,25 +6,49 @@ import apiUrl from '../../apiConfig'
 import Layout from '../shared/Layout'
 
 const Forager = props => {
-  const [forager, setForager] = useState(null)
+  const [foragers, setForagers] = useState(null)
   const [skill, setSkill] = useState('')
 
   // call this callback once after first render, only occurs once
   // because dependency array is empty, so dependencies never change
   // similar to componentDidMount
   useEffect(() => {
-    axios(`${apiUrl}/foragers/${props.match.params.id}`)
+    axios(`${apiUrl}/foragers`)
       // make sure to updated this.setState to hooks setForager
       .then(res => {
-        setForager(res.data.forager)
-        if (res.data.forager.skill) {
-          axios(`${apiUrl}/skills/${res.data.forager.skill}`)
-            .then(res => setSkill(res.data.skill))
-            .catch()
-        }
+        setForagers(res.data.foragers)
       })
       .catch()
   }, [])
+
+  const foragerss = foragers.map(forager => {
+    const fighter1Selector = () => {
+      // setFighter1(forager._id)
+      // setFighter1Skill(forager.skill)
+      const forager1 = forager
+    }
+    const fighter2Selector = () => {
+      // setFighter2(forager._id)
+      // setFighter2Skill(forager.skill)
+      const forager2 = forager
+      socket.emit('new peep', { fighter: { forager2 } })
+    }
+
+    return (
+      <tbody className="lay" key={forager._id}>
+        <tr>
+          <td><Link to={`/foragers/${forager._id}`}>{forager.name}<br></br></Link>
+            {props.user._id === game.player1 && (game.player2) ? <Button variant="secondary" onClick={fighter1Selector}>Forager 1</Button> : ''}
+            {props.user._id === game.player2 ? <Button variant="secondary" onClick={fighter2Selector}>Forager 2</Button> : ''}
+          </td>
+          <td>{forager.description}</td>
+          <td>{forager.hp}</td>
+          <td>{forager.mp}</td>
+          <td>{forager.str}</td>
+        </tr>
+      </tbody>
+    )
+  })
 
   if (!forager) {
     return <p>Loading...</p>
